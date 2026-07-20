98e3af36b1325f285df57769baec5cd373bbe72a
KaltMax <mkaltenr@gmx.at>
2026-07-20 12:13:59 +0200

    feat: add cors to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import express, { type Express, type Request, type Response } from 'express';
 import { round } from './data/quiz.ts';
+import cors from 'cors';
 
 const app: Express = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
